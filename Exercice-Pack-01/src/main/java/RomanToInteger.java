9f894dd26ffd7ad4a2499fb3ff849f78f4400a47
George Alan <georgealanrufo@gmail.com>
2022-06-21 00:09:38 -0300

    add new exercice to do, 13. Roman to Integer

---
+++ Exercice-Pack-01/src/main/java/RomanToInteger.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String romanNumber = scanner.nextLine();
+
+        int conversionResult = romanNumberToInteger(romanNumber);
+        System.out.println(conversionResult);
+    }
+
+    private static int romanNumberToInteger(String s) {
+
+
+        return 1;
+    }
+
+}
