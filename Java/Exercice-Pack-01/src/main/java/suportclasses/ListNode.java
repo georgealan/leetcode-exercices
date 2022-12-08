7ba7f10cd2161f05a2c1b2bc2277d37589516433
George Alan <georgealanrufo@gmail.com>
2022-12-08 00:00:29 -0300

    Organizing files in Intellij

--- Java/Exercice-Pack-01/src/main/java/suportclasses/ListNode.java
+++ Java/Exercice-Pack-01/src/main/java/suportclasses/ListNode.java
@@ -3,19 +3,16 @@ package suportclasses;
 public class ListNode {
     public int val;
     public ListNode next;
-
-    public ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+    public ListNode() {
+        this.val = 0;
+        this.next = null;
     }
-
     public ListNode(int val) {
         this.val = val;
         this.next = null;
     }
-
-    public ListNode() {
-        this.val = 0;
-        this.next = null;
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
--- Java/Exercice-Pack-01/src/main/java/suportclasses/TreeNode.java
+++ Java/Exercice-Pack-01/src/main/java/suportclasses/TreeNode.java
@@ -1,9 +1,22 @@
 package suportclasses;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {
+        this.val = 0;
+        this.left = null;
+        this.right = null;
+    }
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
