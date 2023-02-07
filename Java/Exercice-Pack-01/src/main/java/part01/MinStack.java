64e0929d14ec3297a6e445314fb668ab2c6ba425
George Alan <georgealanrufo@gmail.com>
2023-02-07 16:06:32 -0300

    Finished exercise 155

--- Java/Exercice-Pack-01/src/main/java/part01/MinStack.java
+++ Java/Exercice-Pack-01/src/main/java/part01/MinStack.java
@@ -1,29 +1,37 @@
 package part01;
 
+import java.util.Stack;
+
 /*
 TODO Leetcode exercise 155. Min Stack
  */
 public class MinStack {
+    Stack<Integer> sk = new Stack<>();
+    Stack<Integer> min = new Stack<>();
     public MinStack() {
 
     }
 
     public void push(int val) {
-
+        sk.push(val);
+        if (min.isEmpty() || min.peek() >= val) {
+            min.push(val);
+        }
     }
 
     public void pop() {
-
+        int pop = sk.pop();
+        if (pop == min.peek()) {
+            min.pop();
+        }
     }
 
     public int top() {
-
-        return 1;
+        return sk.peek();
     }
 
     public int getMin() {
-
-        return 1;
+        return min.peek();
     }
 
     public static void main(String[] args) {
