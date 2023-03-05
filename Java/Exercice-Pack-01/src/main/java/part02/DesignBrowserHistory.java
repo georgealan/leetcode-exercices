cdeeb8fea0549b3005394016a7b1361e2a09a166
George Alan <georgealanrufo@gmail.com>
2023-03-04 21:37:35 -0300

    Continue working in the solution

--- Java/Exercice-Pack-01/src/main/java/part02/DesignBrowserHistory.java
+++ Java/Exercice-Pack-01/src/main/java/part02/DesignBrowserHistory.java
@@ -5,24 +5,67 @@ package part02;
   Link: https://leetcode.com/problems/design-browser-history/
 */
 
-import suportclasses.ListNode;
+
 
 class BrowserHistory {
-    String homepage;
+    class Node {
+        String homepage;
+        Node prev;
+        Node next;
+        Node(String homepage) {
+            this.homepage = homepage;
+            this.prev = null;
+            this.next = null;
+        }
+    }
     int lenght;
-    ListNode node = null;
+    Node browser;
     public BrowserHistory(String homepage) {
-        this.homepage = homepage;
-        this.lenght = 0;
+        this.browser = new Node(homepage);
+        this.lenght = 1;
     }
 
     public void visit(String url) {
+        /*
+        Here have to implement insert in the middle when visit back or forward in
+        the middle of the node. It will be behave like insertion in the middle.
+         */
+        Node newVisit = new Node(url);
+        Node lastVisit = browser;
 
+        while (lastVisit.next != null) {
+            lastVisit = lastVisit.next;
+        }
+
+        lastVisit.next = newVisit;
+        newVisit.next = null;
+        newVisit.prev = lastVisit;
+        lenght++;
     }
 
     public String back(int steps) {
+        /*
+        The node move and still in the same position picked here.
+         */
+        Node browserHistory = browser;
+        String url = "";
 
-        return "";
+        System.out.println(browserHistory.next.next.prev.homepage);
+
+        while (browserHistory.next != null) {
+            browserHistory = browserHistory.next;
+        }
+
+        while (steps > 0) {
+            browserHistory = browserHistory.prev;
+            steps--;
+
+            if (steps == 0) {
+                url = browserHistory.homepage;
+            }
+        }
+        System.out.println("Result of bck() method: " + url);
+        return url;
     }
 
     public String forward(int steps) {
@@ -32,13 +75,34 @@ class BrowserHistory {
 }
 public class DesignBrowserHistory {
     public static void main(String[] args) {
+        BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+
+        browserHistory.visit("google.com");
+        browserHistory.visit("facebook.com");
+        browserHistory.visit("youtube.com");
+        browserHistory.visit("ardanlabs.com");
+        printLinkedList(browserHistory.browser);
+
+        browserHistory.back(2);
+        browserHistory.back(1);
+        printLinkedList(browserHistory.browser);
+
+        /*browserHistory.forward(1);
+        browserHistory.visit("linkedin.com");
+        browserHistory.forward(2);
+        browserHistory.back(2);
+        browserHistory.back(7);*/
+
+        System.out.println("\nValues:");
+        System.out.println("Lenght value: " + browserHistory.lenght);
+
 
     }
 
-    public static void printLinkedList(ListNode head) {
-        ListNode curr = head;
+    public static void printLinkedList(BrowserHistory.Node head) {
+        BrowserHistory.Node curr = head;
         while (curr != null) {
-            System.out.print(curr.val + " -> ");
+            System.out.print(curr.homepage + " -> ");
             curr = curr.next;
         }
         System.out.println();
